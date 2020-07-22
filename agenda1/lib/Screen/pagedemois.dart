d01b0f966cb3cd0ed9358d555b3b1edc8c0c2fd8
pentest225 <47216215+pentest225@users.noreply.github.com>
2020-07-22 10:32:45 +0000

    --- Auto-Git Commit ---

--- agenda1/lib/Screen/pagedemois.dart
+++ agenda1/lib/Screen/pagedemois.dart
@@ -1,8 +1,26 @@
 import '../Providers/data.dart';
 import '../Widgets/lesDates.dart';
 import 'package:flutter/material.dart';
+import 'package:date_util/date_util.dart';
 
-class Lemois extends StatelessWidget {
+class Lemois extends StatefulWidget {
+  @override
+  _LemoisState createState() => _LemoisState();
+}
+
+class _LemoisState extends State<Lemois> {
+  
+
+  void generateAgendat(DateTime myDate){
+    var myDateUtility = DateUtil();
+    var nbrDayInMonth = myDateUtility.daysInMonth(myDate.month, myDate.year);
+    var totalDayFirst = myDateUtility.totalLengthOfDays(myDate.month, (nbrDayInMonth - nbrDayInMonth + 1), myDate.year);
+    var totalDayLast = myDateUtility.totalLengthOfDays(myDate.month, nbrDayInMonth, myDate.year);
+    String startDay = myDateUtility.day(totalDayFirst);
+    String endDay = myDateUtility.day(totalDayLast);
+    
+
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
