c124da46b7655d00a7cb67fe9b72fdefa74268cf
pentest225 <47216215+pentest225@users.noreply.github.com>
2020-07-22 11:02:59 +0000

    --- Auto-Git Commit ---

--- agenda1/lib/Screen/pagedemois.dart
+++ agenda1/lib/Screen/pagedemois.dart
@@ -10,19 +10,57 @@ class Lemois extends StatefulWidget {
 
 class _LemoisState extends State<Lemois> {
   
+  int daysBeforeStart(String day){
+    int index = 0;
+    switch (day) {
+      case "Monday":
+        index = 0;
+        break;
+      case "Tuesday":
+        index = 1;
+        break;
+      case "Wednesday":
+        index = 2;
+        break;
+      case "Thursday":
+        index = 3;
+        break;
+      case "Friday":
+        index = 4;
+        break;
+      case "Saturday":
+        index = 5;
+        break;
+      case "Sunday":
+        index = 6;
+        break;
+      default:
+        index = 0;
+    }
+    return index;
 
+  }
   void generateAgendat(DateTime myDate){
+    List<int> myCalendar = [];
     var myDateUtility = DateUtil();
     var nbrDayInMonth = myDateUtility.daysInMonth(myDate.month, myDate.year);
+    var nbrDayLastMonth = myDateUtility.daysInMonth(myDate.month - 1, myDate.year);
     var totalDayFirst = myDateUtility.totalLengthOfDays(myDate.month, (nbrDayInMonth - nbrDayInMonth + 1), myDate.year);
     var totalDayLast = myDateUtility.totalLengthOfDays(myDate.month, nbrDayInMonth, myDate.year);
     String startDay = myDateUtility.day(totalDayFirst);
     String endDay = myDateUtility.day(totalDayLast);
-    
+   
+    print("Nombre de jour dans le moi $nbrDayInMonth");
+    print("The Start Day $startDay");
+    print("The End Day $endDay");
+    for (var i = 0; i < daysBeforeStart(startDay); i++) {
+      myCalendar.add(nbrDayLastMonth - )
+    }
 
   }
   @override
   Widget build(BuildContext context) {
+    generateAgendat(DateTime.now());
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
